d2520ef5b9dbbb59d1c1e53fe4c19c2f744e35eb
victorandresp <ponceottamendi27@gmail.com>
2024-05-16 19:44:11 -0400

    feat: return both columns in one with alias :sparkles:

--- getters/students.sql
+++ getters/students.sql
@@ -1,1 +1,6 @@
-SELECT num_control, first_name, last_name, average  FROM students WHERE average >= 9 ORDER BY average ASC;
+/* retrieve data for students with average more than 90 and ordered by minor average to major*/
+SELECT num_control, first_name, last_name, average  FROM students WHERE average >= 9O ORDER BY average ASC;
+
+/* same query but returns all the name in one column */
+
+SELECT num_control, first_name|| ' ' || last_name AS full_name, average  FROM students WHERE average >= 90 ORDER BY average ASC;
