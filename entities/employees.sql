d2a20a0dea726749a8743bd80acc4db2a4cc3d87
victorandresp <ponceottamendi27@gmail.com>
2024-05-15 22:11:01 -0400

    feat: update a column by condition :sparkles:

--- entities/employees.sql
+++ entities/employees.sql
@@ -10,5 +10,7 @@ CREATE TABLE employees (
 INSERT INTO employees(num_employee, first_name, last_name, birthdate, salary, job_position)
 VALUES 
 (1, 'Victor', 'Ponce', '1997-10-27',  4200.8, 'Contador'),
-(2, 'Andres', 'Ottamendi', '1997-10-2',  3800.2, 'Seecretario'),
+(2, 'Andres', 'Ottamendi', '1997-10-2',  3800.2, 'Secretario'),
 (3, 'Daniel', 'Garcia', '1999-11-22',  6000, 'Gerente');
+/* update a unique field by id*/
+UPDATE employees SET job_position = 'Secretario' WHERE num_employee = 2;
