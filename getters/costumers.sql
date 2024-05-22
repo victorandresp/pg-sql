f7681c5e2a3253a385236bcb51d77d72cfa85e27
victorandresp <ponceottamendi27@gmail.com>
2024-05-21 21:59:34 -0400

    feat: delete permanently table :sparkles:

--- getters/costumers.sql
+++ getters/costumers.sql
@@ -3,9 +3,13 @@
 SELECT first_name ||' '|| last_name as full_name, debt_total, state FROM costumers
 WHERE debt_total > 10000 AND state = 'Sinaloa';
 
-/* DELETE COMPLETE TABLE*/
+/* CLEAR COMPLETE TABLE*/
 
 DELETE FROM costumers;
 
 /* DELETE SINGLE FILE BY CONDITIONAL*/
 DELETE FROM costumers WHERE id_costumer = 2 ;
+
+/* DELETE PERMANENTLY COMPLETE TABLE*/
+
+DROP TABLE IF EXISTS costumers, students;
