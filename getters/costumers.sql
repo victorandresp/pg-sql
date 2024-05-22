979b8e5cbd03fd8ed7d78d7127a8b8d27682c21e
victorandresp <ponceottamendi27@gmail.com>
2024-05-21 21:54:14 -0400

    feat: delete queries :sparkles:

--- getters/costumers.sql
+++ getters/costumers.sql
@@ -2,3 +2,10 @@
 
 SELECT first_name ||' '|| last_name as full_name, debt_total, state FROM costumers
 WHERE debt_total > 10000 AND state = 'Sinaloa';
+
+/* DELETE COMPLETE TABLE*/
+
+DELETE FROM costumers;
+
+/* DELETE SINGLE FILE BY CONDITIONAL*/
+DELETE FROM costumers WHERE id_costumer = 2 ;
