332274f89c8b0855e0c253eb77e7ddcaf76335e0
victorandresp <ponceottamendi27@gmail.com>
2024-05-22 22:19:59 -0400

    chore: comments :memo:

--- getters/costumers.sql
+++ getters/costumers.sql
@@ -3,7 +3,7 @@
 SELECT first_name ||' '|| last_name as full_name, debt_total, state FROM costumers
 WHERE debt_total > 10000 AND state = 'Sinaloa';
 
-/* CLEAR COMPLETE TABLE*/
+/* CLEAR COMPLETE TABLE BUT LEAVE COLUMNS CREATED*/
 
 DELETE FROM costumers;
 
