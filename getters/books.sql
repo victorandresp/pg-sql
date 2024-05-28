3e6cd2ccd16643ad80f196c5c091a6ce9776203e
victorandresp <ponceottamendi27@gmail.com>
2024-05-27 23:10:53 -0400

    feat: get max value by a group by :sparkles:

--- getters/books.sql
+++ getters/books.sql
@@ -1,2 +1,7 @@
 -- COUNT BOOKS BY EDITORIAL 
 SELECT editorial, COUNT(*) AS quantity_by_editorial FROM books GROUP BY editorial ORDER BY quantity_by_editorial DESC
+-- GET THE MOST EXPENSIVE BOOK BY EDITORIAL
+
+SELECT editorial, MAX(price) AS most_expensive FROM books
+GROUP BY editorial
+ORDER BY most_expensive DESC;
